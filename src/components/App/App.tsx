c0ebe65d9b6881c9f29d462c29d419ccfadfe83a
smelik81 <artem.smelyanskiy81@gmail.com>
2025-12-28 17:07:21 +0200

    added components SearchBar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,29 @@
 import "./App.module.css";
+import toast from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import type { Movie } from "../../types/movie";
 import movieService from "../../services/movieService";
-import { useEffect } from "react";
+import SearchBar from "../SearchBar/SearchBar";
 
 function App() {
-  useEffect(() => {
-    async function fetchMovies() {
-      const data = await movieService("Batman");
-      console.log("TMDB response:", data);
+  const [movie, setMovie] = useState<Movie[]>([]);
+
+  const handleSearch = async (query: string) => {
+    setMovie([]);
+    const data = await movieService(query);
+    if (movie.length === 0) {
+      toast.error("No movies found for your request.");
+      return;
     }
-    fetchMovies();
-  }, []);
-  return <></>;
+    setMovie(data);
+  };
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster position="top-right" />
+    </>
+  );
 }
 
 export default App;
